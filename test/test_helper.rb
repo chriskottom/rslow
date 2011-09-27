aee0341d433080411cc090dcfe6f0efcc65f904f
Chris Kottom <chris@chriskottom.com>
2011-09-27 12:27:16 +0200

    refactoring TestHelper, moving test inputs (html, css) to separate files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,19 +4,44 @@ require "test/unit"
 require "rslow"
 
 module TestHelper
-  HTTP_RESPONSE = {
-    code:         "200",
-    message:      "OK",
-    body:         "<html><title>Test</title><body>Page Body</body></html>"
-  }
+  TEST_URL          = "http://www.ruby-lang.org/en/"
+  TEST_RELATIVE_URL = "/images/download.gif"
+
+  TEST_CSS_FILE     = File.join(File.dirname(__FILE__), "test.css")
+  TEST_HTML_FILE    = File.join(File.dirname(__FILE__), "test.html")
+
+  TEST_CSS          = File.read(TEST_CSS_FILE)
+  TEST_HTML         = File.read(TEST_HTML_FILE)
+
+  HTTP_SUCCESS      = { code: "200", message: "OK" }
+  GIF_SUCCESS       = HTTP_SUCCESS.merge({ :"Content-Type" => "image/gif" })
+
 
   def mock_http_response
     http_mock = mock("Net::HTTPResponse")
-    http_mock.stubs(code:     HTTP_RESPONSE[:code],
-                    message:  HTTP_RESPONSE[:message],
-                    body:     HTTP_RESPONSE[:body],
-                    to_hash:  HTTP_RESPONSE)
+    http_mock.stubs(code:     HTTP_SUCCESS[:code],
+                    message:  HTTP_SUCCESS[:message],
+                    body:     TEST_HTML,
+                    to_hash:  HTTP_SUCCESS)
     http_mock
+  end
+
+  def mock_css_response
+    css_mock = mock("Net::HTTPResponse")
+    css_mock.stubs(code:     HTTP_SUCCESS[:code],
+                   message:  HTTP_SUCCESS[:message],
+                   body:     TEST_CSS,
+                   to_hash:  HTTP_SUCCESS)
+    css_mock
+  end
+
+  def mock_image_resource
+    image_mock = mock("RSlow::Resources::Basic_Resource")
+    image_mock.stubs(code:         GIF_SUCCESS[:code],
+                     message:      GIF_SUCCESS[:message],
+                     body:         "",
+                     content_type: GIF_SUCCESS[:"Content-Type"])
+    image_mock
   end
 end
 
