ef2f1676433a88f38aaee995abf4f118ba0874f0
Chris Kottom <chris@chriskottom.com>
2011-09-27 16:38:10 +0200

    minor cleanup of RSlow module code

--- lib/rslow/rslow.rb
+++ lib/rslow/rslow.rb
@@ -1,52 +1,53 @@
 module RSlow
-  extend self
-
-  def configure(&block)
-    instance_eval(&block)
-  end
-
-  def ruleset(label, &block)
-    ruleset = Ruleset.new(label)
-    ruleset.instance_eval(&block) if block_given?
-    self.rulesets[ruleset.label] = ruleset
-    ruleset
-  end
-
-  def rulesets
-    @rulesets ||= {}
-  end
-
-  def page(label, url)
-    pages[label] = RSlow::Resources::HtmlResource.new(url)
-  end
-
-  def pages
-    @pages ||= {}
-  end
-
-  def evaluate(page_label, ruleset_label)
-    page = pages[page_label]
-    ruleset = rulesets[ruleset_label]
-    validate_page_and_ruleset(page, ruleset, page_label, ruleset_label)
-
-    ruleset.evaluate(page)
-  end
-
-  private
-  def validate_page_and_ruleset(page, ruleset, page_label, ruleset_label)
-    return if page && ruleset
-
-    message = nil
-    if page.nil && ruleset.nil?
-      message = "Neither page nor ruleset was found:  " +
-                "page => #{ page_label }, ruleset => #{ ruleset_label }"
-    elsif page.nil?
-      message = "Page not found for label:  #{ page_label }"
-    elsif ruleset.nil?
-      message = "Ruleset not found for label:  #{ ruleset_label }"
+  class << self
+    def configure(&block)
+      instance_eval(&block)
     end
 
-    raise ArgumentError, message
+    def ruleset(label, &block)
+      rules = Ruleset.new(label)
+      rules.instance_eval(&block) if block_given?
+      self.rulesets[rules.label] = rules
+    end
+
+    def rulesets
+      @rulesets ||= {}
+    end
+    
+    def page(label, url)
+      pages[label] = RSlow::Resources::HtmlResource.new(url)
+    end
+
+    def pages
+      @pages ||= {}
+    end
+
+    def evaluate(page_label, ruleset_label)
+      page = pages[page_label]
+      ruleset = rulesets[ruleset_label]
+      validate_page_and_ruleset(page, ruleset, page_label, ruleset_label)
+      
+      ruleset.evaluate(page)
+    end
+
+    private
+
+    # Check whether the page_label and ruleset_label correspond to objects known
+    # to RSlow.  Raise an appropriate exception if this is not the case.
+    def validate_page_and_ruleset(page, ruleset, page_label, ruleset_label)
+      return if page && ruleset
+
+      message = nil
+      if page.nil && ruleset.nil?
+        message = "Neither page nor ruleset was found:  " +
+          "page => #{ page_label }, ruleset => #{ ruleset_label }"
+      elsif page.nil?
+        message = "Page not found for label:  #{ page_label }"
+      elsif ruleset.nil?
+        message = "Ruleset not found for label:  #{ ruleset_label }"
+      end
+
+      raise ArgumentError, message
+    end
   end
 end
-
