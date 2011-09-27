bc06237f2b8c351d5c931c34e0eda25444290ee2
Chris Kottom <chris@chriskottom.com>
2011-09-27 16:20:23 +0200

    cleanup of rslow.rb requires

--- lib/rslow.rb
+++ lib/rslow.rb
@@ -2,13 +2,15 @@ $:.unshift(File.dirname(__FILE__))
 
 require "rslow/rslow"
 
+require "rslow/ruleset"
+require "rslow/grading"
+
+require "rslow/rule"
+require "rslow/rules/request_count_rule"
+require "rslow/rules/gzip_rule"
+
 require "rslow/resource"
 require "rslow/resources/basic_resource"
 require "rslow/resources/html_resource"
 require "rslow/resources/css_resource"
 require "rslow/resources/js_resource"
-
-require "rslow/ruleset"
-require "rslow/rule"
-require "rslow/rules/request_count_rule"
-require "rslow/grading"
